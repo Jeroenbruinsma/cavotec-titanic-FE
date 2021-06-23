c3117e1b4dc1fd5a7def243c3fa3060fec188bd6
Jeroenbruinsma <jeroenbruinsma@gmail.com>
2021-06-23 17:14:09 +0200

    Simple homepage

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import { Container } from "@material-ui/core";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
@@ -13,57 +14,42 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to{" "}
+          <a href="https://nl.wikipedia.org/wiki/Titanic_(schip,_1912)">
+            Titanic
+          </a>
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
+        <p className={styles.description}>Passenger managment system</p>
+        <Container maxWidth="sm">
+          <img src="/images/titanic.jpg" alt="boat" />
+        </Container>
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+          <a
+            href="https://github.com/Jeroenbruinsma/cavotec-titanic-FE"
+            className={styles.card}
+          >
+            <h2>Frontend Repo &rarr;</h2>
+            <p className={styles.nextJs}>
+              Discover how the frontend app is made using{" "}
+              <a href="https://nextjs.org">NextJs</a>
+            </p>
           </a>
 
           <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
+            href="https://github.com/Jeroenbruinsma/cavotec-titanic"
             className={styles.card}
           >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+            <h2>Backend repo &rarr;</h2>
+            <p className={styles.nextJs}>
+              Discover how the backend code is written using{" "}
+              <a href="https://nodejs.org/en/">NodeJS</a>,{" "}
+              <a href="https://expressjs.com/">Express</a> &{" "}
+              <a href="https://sequelize.org/">Sequelize</a>
             </p>
           </a>
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
-  )
+  );
 }
