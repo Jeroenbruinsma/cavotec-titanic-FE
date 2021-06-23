9e9fd2687a3896b0cd3ab3ac0cb62387628ff2ce
Jeroenbruinsma <jeroenbruinsma@gmail.com>
2021-06-23 12:55:32 +0200

    style page with material ui

--- pages/passenger/[id].js
+++ pages/passenger/[id].js
@@ -1,18 +1,55 @@
 import axios from "axios";
+import { makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
 
 export default function index({person}) {
-    console.log("pass", person)
-    return (
-        <div>
-           <h1> Name: {person.name}</h1>
-           <h2> Age: {person.age}</h2>
-           <h2> Sex: {person.sex}</h2>
-           <h2> Survived: {person.survived}</h2>
-           <h2> Class: {person.passengerClass}</h2>
-           <h2> Parents/Children aboard {person.parentsOrChildrenAboard}</h2>
-           <h2> Siblings/spouses aboard {person.siblingsOrSpousesAboard}</h2>
-        </div>
-    )
+    const useStyles = makeStyles({
+        table: {
+          minWidth: 650,
+        },
+      });
+      const classes = useStyles();
+      return (
+        <TableContainer component={Paper}>
+          <Table className={classes.table} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Property</TableCell>
+                <TableCell align="right">Value</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {Object.keys(person).map((property) => {
+              return(
+                <TableRow key={property.name}>
+                  <TableCell component="th" scope="row">
+                    {property}
+                  </TableCell>
+                  <TableCell align="right">{JSON.stringify(person[property])}</TableCell>
+                </TableRow>
+              )})}
+                
+            </TableBody>
+          </Table>
+        </TableContainer>
+      );
+      // return (
+    //     <div>
+    //        <h1> Name: {person.name}</h1>
+    //        <h2> Age: {person.age}</h2>
+    //        <h2> Sex: {person.sex}</h2>
+    //        <h2> Survived: {person.survived}</h2>
+    //        <h2> Class: {person.passengerClass}</h2>
+    //        <h2> Parents/Children aboard {person.parentsOrChildrenAboard}</h2>
+    //        <h2> Siblings/spouses aboard {person.siblingsOrSpousesAboard}</h2>
+    //     </div>
+    // )
 }
 
 export async function getServerSideProps({ params }) {
