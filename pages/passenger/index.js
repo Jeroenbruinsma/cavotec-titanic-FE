9b5eecfd21c1e5e1898ac632bf77d81884071a0d
Jeroenbruinsma <jeroenbruinsma@gmail.com>
2021-06-23 12:35:26 +0200

    install material ui

--- pages/passenger/index.js
+++ pages/passenger/index.js
@@ -1,22 +1,54 @@
 import axios from "axios";
+import { makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
 
-export default function index({passengers}) {
-    console.log("pass", passengers)
-    return (
-        <div>
-           {passengers.map(p => <h1>{p.name}</h1>)}         
-        </div>
-    )
+export default function index({ passengers }) {
+  const useStyles = makeStyles({
+    table: {
+      minWidth: 650,
+    },
+  });
+  const classes = useStyles();
+
+  return (
+    <TableContainer component={Paper}>
+      <Table className={classes.table} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Name</TableCell>
+            <TableCell align="right">Age</TableCell>
+            <TableCell align="right">Gender</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {passengers.map((row) => (
+            <TableRow key={row.id}>
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">{row.age}</TableCell>
+              <TableCell align="right">{row.sex}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 }
 
 export async function getServerSideProps({ params }) {
-    try{
-        const response = await axios.get(`http://localhost:5000/people`);
-        const data = response.data
-        return {props: { passengers: data }}
-    }catch(err){
-        console.log("error" , err)
-        return {props: { passengers: []}}
-    }
-
+  try {
+    const response = await axios.get(`http://localhost:5000/people`);
+    const data = response.data;
+    return { props: { passengers: data } };
+  } catch (err) {
+    console.log("error", err);
+    return { props: { passengers: [] } };
+  }
 }
